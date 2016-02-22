366cb0a253bc5e99101aec759bc23cfebfdf4fbb
Edward Heyne <eheyne@virtualhold.com>
2016-02-22 16:53:35 -0500

    Added in more unit tests to support jquery plugin behavior and added the svg element to the donut

--- donut.js
+++ donut.js
@@ -6,6 +6,22 @@
   }
 }(function($) {
   var Donut = function() {
+    
+    var $this = $(this);
+
+    if ($this.length > 0) {
+      $this.append('<svg />');
+      var svgElements = $this.find('svg');
+
+      svgElements.each(function(index) {
+        var svgElement = svgElements[index];
+        svgElement.setAttribute('width', $this.width());
+        svgElement.setAttribute('height', $this.height());
+      });
+    } else {
+      console.log('No elements found in selector', this.selector);
+    }
+
     return this;
   };
 
--- tests/donutTest.js
+++ tests/donutTest.js
@@ -1,14 +1,60 @@
 var expect = chai.expect;
 
 describe('donut', function() {
+  var donut;
+  beforeEach(function() {
+    donut = $('#donut').Donut();
+  });
+
+  afterEach(function() {
+    if (donut) {
+      donut.empty();
+    }
+  });
+
+  it('should handle if no elements selected', function() {
+    var empty = $('#empty').Donut();
+    expect(empty.length).to.equal(0);
+  });
+
   it('should be a typeof object', function() {
-    var donut = $('#donut').Donut();
     expect(typeof donut).to.equal('object');
   });
 
   it('should support chaining', function() {
-    var donut = $('#donut').Donut().append('<h1>Test</h1>');
+    donut = $('#donut').Donut().append('<h1>Test</h1>');
     var h1 = $('#donut').find('h1');
     expect(h1.text()).to.equal('Test');
+  });
+
+  it('should create one svg element', function() {
+    var svg = $('#donut').find('svg');
+    expect(svg.length).to.equal(1);
+    expect(svg[0]).to.exist;
+  });
+
+  it('should handle multiple items returned from selector', function() {
+    
+    var donuts = $('.donuts').Donut();
+    donuts.each(function(index) { 
+      expect(donuts[index].children.length).to.equal(1);
+
+      var svg = $(donuts[index]).find('svg');
+      var offset = 4;  // don't know why its 4 off.
+      expect(donut.width()).to.equal(svg.width());
+      var height = svg.height();
+      expect(donut.height()).to.be.within(height - offset, height + offset);
+    });
+
+    donuts.each(function(index) {
+      $(donuts[index]).empty();
+    });
+  });
+
+  it('should contain an svg element with the width and height of its parent', function() {
+    var svg = $('#donut').find('svg');
+    var offset = 4;  // don't know why its 4 off.
+    expect(donut.width()).to.equal(svg.width());
+    expect(donut.height()).to.equal(svg.height() + offset);
   });
 });
