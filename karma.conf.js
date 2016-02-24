bd72485f978dc87c3a176f27e8a8575b7548c068
Edward Heyne <eheyne@virtualhold.com>
2016-02-24 14:12:10 -0500

    Integrated karma as a test runner

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+    basePath: './',
+    frameworks: ['mocha'],
+    files: [
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/mocha/mocha.js',
+      'node_modules/chai/chai.js',
+      {pattern: 'donut.js', watched: true},
+      {pattern: 'test/unit/*.spec.js', watched: true}],
+    exclude: [],
+    browsers: ['PhantomJS']
+  });
+};
+
