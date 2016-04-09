37cc7fa752b34f78b708d0826d258ce305caabea
Edward Heyne <eheyne@virtualhold.com>
2016-04-09 12:38:39 -0400

    Added api endpoint to feed back array of random data

--- tasks/server.js
+++ tasks/server.js
@@ -1,12 +1,24 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var fileToHost = process.argv[2] || 'public';
 
 app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(path.join(__dirname, fileToHost)));
+app.use(bodyParser.json());
+
+function randomize() {
+  return Math.floor(Math.random() * 25 + 1);
+}
+
+app.get('/api/data', function(req, res) {
+  res.json([randomize(), randomize(), randomize(), randomize()]);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
+
